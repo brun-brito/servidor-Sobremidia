3b87393b6b51d743c5aa50f383c6f91f9115ead1
Bruno Brito <bruno-brito@MacBook-de-Bruno.local>
2025-03-05 09:35:46 -0300

    Criação de cron job no firebase que verifica inserções e envia e-mail todo dia as 02AM

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 const functions = require("firebase-functions");
 const app = require("./src/app");
+const { onSchedule } = require("firebase-functions/v2/scheduler");
+const { generateDailyReport, waitForReport, processReportResult } = require("./src/services/verificaInsercoesService");
+
+exports.verifica_insercoes = onSchedule(
+    {
+        schedule: "0 2 * * *", // Todos os dias às 02:00
+        timeZone: "America/Sao_Paulo", // Timezone SP
+        retryCount: 5, // Tentará rodar novamente até 3 vezes em caso de falha
+        maxRetrySeconds: 180, // Tempo máximo de re-tentativas (3 minutos)
+        minBackoffSeconds: 30, // Tempo mínimo entre tentativas (30s)
+        maxBackoffSeconds: 120, // Tempo máximo entre tentativas (2 minutos)
+        maxDoublings: 2, // O tempo entre re-tentativas dobrará até 2 vezes
+    },
+    async (event) => {
+        console.log("[INFO] Iniciando função verifica_insercoes...");
+
+        try {
+            console.log("[INFO] Gerando relatório diário...");
+            const reportData = await generateDailyReport();
+            console.log("[INFO] Report ID:", reportData?.reportId);
+
+            if (!reportData || !reportData.reportId) {
+                console.error("[ERROR] Nenhum relatório gerado.");
+                return;
+            }
+
+            console.log("[INFO] Aguardando resultado do relatório...");
+            const reportResult = await waitForReport(reportData.reportId);
+            console.log("[INFO] Resultado recebido.");
+
+            console.log("[INFO] Processando relatório...");
+            await processReportResult(reportResult, reportData.mediaToDetailsMap);
+
+            console.log("[INFO] Processo concluído com sucesso!");
+        } catch (error) {
+            console.error("[ERROR] Erro inesperado:", error);
+        }
+    }
+);
+
 
 exports.v1 = functions.https.onRequest(app);
 
