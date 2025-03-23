96780398e8b364bf4ebb7de094d230450357ade9
Bruno Brito <brunocaudebrito@hotmail.com>
2025-03-22 23:33:31 -0300

    Separando funcoes e organizando rotas

--- src/controllers/analyticsController.js
+++ src/controllers/analyticsController.js
@@ -1,4 +1,6 @@
 const { fetchAnalyticsData } = require('../services/analyticsService');
+const { db } = require("../config/firebase");
+const { updateDailyAnalytics } = require("../utils/dailyAnalyticsUpdate");
 
 exports.generateAnalyticsData = async (req, res) => {
   try {
@@ -17,3 +19,27 @@ exports.generateAnalyticsData = async (req, res) => {
     res.status(500).json({ error: 'Erro ao buscar dados de analytics.' });
   }
 };
+
+exports.updateDailyAnalytics = async (req, res) => {
+  try {
+    console.log("[INFO] atualizando analytics Manualmente...");
+    await updateDailyAnalytics();
+    console.log("[INFO] Processo concluído com sucesso!");
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("[ERROR] Erro inesperado:", error);
+    res.status(500).json({ error: 'Erro ao atualizar analytics.' });
+  }
+};
+
+exports.listAllPaineis = async (req, res) => {
+  try {
+    const snapshot = await db.collection("analytics").doc("paineis").collection("lista").get();
+    const paineis = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    res.status(200).json(paineis);
+  } catch (error) {
+    console.error('Erro ao buscar paineis:', error.message);
+    res.status(500).json({ error: 'Erro ao buscar paineis.' });
+  }
+}
--- src/routes/analytics.js
+++ src/routes/analytics.js
@@ -1,32 +1,13 @@
 const express = require("express");
-const { db, auth } = require("../config/firebase");
-const { updateDailyAnalytics } = require("../utils/dailyAnalyticsUpdate");
-const { generateAnalyticsData } = require('../controllers/analyticsController');
+const analyticsController = require('../controllers/analyticsController');
 
 const router = express.Router();
 
 // Listar todos os paineis
-router.get("/paineis", async (req, res) => {
-  try {
-      const snapshot = await db.collection("analytics").doc("paineis").collection("lista").get();
-      const paineis = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+router.get("/paineis", analyticsController.listAllPaineis);
 
-      res.status(200).json(paineis);
-  } catch (error) {
-      res.status(500).json({ error: error.message });
-  }
-});
+router.post('/', analyticsController.generateAnalyticsData);
 
-router.post('/', generateAnalyticsData);
-
-router.post("/update-analytics", async (req, res) => {
-  try {
-      console.log("[INFO] atualizando analytics Manualmente...");
-      await updateDailyAnalytics();
-      console.log("[INFO] Processo concluído com sucesso!");
-  } catch (error) {
-      console.error("[ERROR] Erro inesperado:", error);
-  }
-});
+router.post("/update", analyticsController.updateDailyAnalytics);
 
 module.exports = router;
