837e57564a4b62a2802f5fc9da79f89a62e05747
Bruno Brito <brunocaudebrito@hotmail.com>
2025-08-04 02:40:46 -0300

    Comentando a função verifica_insercoes e seu agendamento para desativação temporária

--- index.js
+++ index.js
@@ -4,43 +4,43 @@ const { onSchedule } = require("firebase-functions/v2/scheduler");
 const { generateDailyReport, waitForReport, processReportResult } = require("./src/services/verificaInsercoesService");
 const { updateDailyAnalytics } = require("./src/utils/dailyAnalyticsUpdate");
 
-exports.verifica_insercoes = onSchedule(
-    {
-        schedule: "0 2 * * *", // Todos os dias às 02:00
-        timeZone: "America/Sao_Paulo", // Timezone SP
-        retryCount: 5, // Tentará rodar novamente até 3 vezes em caso de falha
-        maxRetrySeconds: 180, // Tempo máximo de re-tentativas (3 minutos)
-        minBackoffSeconds: 30, // Tempo mínimo entre tentativas (30s)
-        maxBackoffSeconds: 120, // Tempo máximo entre tentativas (2 minutos)
-        maxDoublings: 2, // O tempo entre re-tentativas dobrará até 2 vezes
-        timeoutSeconds: 300, // Tempo máximo de execução (5 minutos)
-    },
-    async (event) => {
-        console.log("[INFO] Iniciando função verifica_insercoes...");
+// exports.verifica_insercoes = onSchedule(
+//     {
+//         schedule: "0 2 * * *", // Todos os dias às 02:00
+//         timeZone: "America/Sao_Paulo", // Timezone SP
+//         retryCount: 5, // Tentará rodar novamente até 3 vezes em caso de falha
+//         maxRetrySeconds: 180, // Tempo máximo de re-tentativas (3 minutos)
+//         minBackoffSeconds: 30, // Tempo mínimo entre tentativas (30s)
+//         maxBackoffSeconds: 120, // Tempo máximo entre tentativas (2 minutos)
+//         maxDoublings: 2, // O tempo entre re-tentativas dobrará até 2 vezes
+//         timeoutSeconds: 300, // Tempo máximo de execução (5 minutos)
+//     },
+//     async (event) => {
+//         console.log("[INFO] Iniciando função verifica_insercoes...");
 
-        try {
-            console.log("[INFO] Gerando relatório diário...");
-            const reportData = await generateDailyReport();
-            console.log("[INFO] Report ID:", reportData?.reportId);
+//         try {
+//             console.log("[INFO] Gerando relatório diário...");
+//             const reportData = await generateDailyReport();
+//             console.log("[INFO] Report ID:", reportData?.reportId);
 
-            if (!reportData || !reportData.reportId) {
-                console.error("[ERROR] Nenhum relatório gerado.");
-                return;
-            }
+//             if (!reportData || !reportData.reportId) {
+//                 console.error("[ERROR] Nenhum relatório gerado.");
+//                 return;
+//             }
 
-            console.log("[INFO] Aguardando resultado do relatório...");
-            const reportResult = await waitForReport(reportData.reportId);
-            console.log("[INFO] Resultado recebido.");
+//             console.log("[INFO] Aguardando resultado do relatório...");
+//             const reportResult = await waitForReport(reportData.reportId);
+//             console.log("[INFO] Resultado recebido.");
 
-            console.log("[INFO] Processando relatório...");
-            await processReportResult(reportResult, reportData.mediaToDetailsMap);
+//             console.log("[INFO] Processando relatório...");
+//             await processReportResult(reportResult, reportData.mediaToDetailsMap);
 
-            console.log("[INFO] Processo concluído com sucesso!");
-        } catch (error) {
-            console.error("[ERROR] Erro inesperado:", error);
-        }
-    }
-);
+//             console.log("[INFO] Processo concluído com sucesso!");
+//         } catch (error) {
+//             console.error("[ERROR] Erro inesperado:", error);
+//         }
+//     }
+// );
 
 exports.atualiza_analytics = onSchedule(
     {
